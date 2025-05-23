6cc7d5a811c7cb0b51e77959fece4013108efa97
nur qurratul aini <ainiafifi2@gmail.com>
2025-05-23 22:56:16 +0800

    update.php

--- exercise 4/student/update.php
+++ exercise 4/student/update.php
@@ -5,12 +5,10 @@ $matric = $_POST['matric'];
 $race = $_POST['race'];
 $gender = $_POST['gender'];
 
-// Sanitize inputs (basic)
 $matric = $conn->real_escape_string($matric);
 $race = $conn->real_escape_string($race);
 $gender = $conn->real_escape_string($gender);
 
-// Update query
 $sql = "UPDATE STUDENT SET Race='$race', Gender='$gender' WHERE Matric='$matric'";
 if ($conn->query($sql) === TRUE) {
     echo "Record updated successfully. <a href='list.php'>Back to list</a>";
